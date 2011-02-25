35be17beff598cc7cacf20bca2449a5ab7b4f932
John McGonegal <mcgonegal@gmail.com>
2011-02-25 00:44:11 -0700

    both problems solved

---
+++ Problem1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HyperPublicProgrammingChallenge
+{
+/*
+Hyperpublic Challenge - Problem 1
+
+Hyperpublic users can add their friends by emailing a photo of them to add@hyperpublic.com. We want to determine a user’s influence on the system by determining how many users he is responsible for. A user’s influence is calculated by giving him 1 point for every user he’s added in addition to the sum of the influence scores of each user that he’s added.
+
+Example: User 0 adds User 1 and User 2. User 1 adds User 3.
+
+User 0’s influence score is 3. (He added two users and one of them added added a third user.)
+User 1's is 1. 
+User 2’s is 0. 
+User 3’s is 0.
+The above scenario is represented by the following input file. Line i is user ID i and position j within the line is marked with an X if user ID i added user ID j. Both row and column indicies are 0-based:
+
+ OXXO
+ OOOX
+ OOOO
+ OOOO
+      
+Use the input file here to determine what the highest influence score is among 100 random Hyperpublic users. To compute the answer to problem 1, append the top 3 influence totals together in descending order. (For example if they are 17, 5, and 3 then submit 1753)
+*/
+    class User
+    {
+        int influence = 0;
+        public string line; // im lazy
+        List<User> users = new List<User>(); // list of users that the users has added
+
+        public User(string l)
+        {
+            line = l;
+        }
+        public void addUser(User u)
+        {
+            users.Add(u);
+        }
+        public int getInfluence()
+        {
+            return influence;
+        }
+        public static int calcInfulence(List<User> users)
+        {
+            int influence = 0;
+            foreach (User u in users)
+            {
+                influence += calcInfulence(u.users);
+            }
+            return users.Count + influence;
+        }
+        public void calc()
+        {
+            influence = calcInfulence(this.users);
+        }
+        public static int CompareInfluence(User a, User b)
+        {
+            return b.getInfluence().CompareTo(a.getInfluence());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("challenge2input.txt");
+            StreamReader fs = new StreamReader(file.OpenRead());
+
+            string line;
+            List<User> users = new List<User>();
+
+            // read our file info and create 1 user for each line
+            while ((line = fs.ReadLine()) != null)
+            {
+                users.Add(new User(line));
+            }
+            fs.Close();
+
+            // read the line info and link users to each user they've added
+            for (int i = 0; i < users.Count; i++)
+            {
+                char[] t = users[i].line.ToCharArray();
+                for (int j = 0; j < t.Length; j++)
+                {
+                    if (t[j] == 'X')
+                    {
+                        users[i].addUser(users[j]);
+                    }
+                }
+            }
+            // calculate influence
+            for (int i = 0; i < users.Count; i++)
+            {
+                users[i].calc();
+            }
+            // sort it
+            users.Sort(User.CompareInfluence);
+
+            
+            // print the answer
+            Console.Write("The answer is ");
+            foreach (User u in users.GetRange(0,3))
+            {
+                Console.Write(u.getInfluence());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem2
+{
+/*
+Hyperpublic has an internal karma system to determine which users are the most involved in the ecosystem. Users earn points for the following tasks.
+
+2 Points – Add Place
+3 Points – Add Thing
+17 Points – Tag Object
+23 Points – Upload Photo
+42 Points – Twitter Share
+98 Points – Facebook Share
+Being addicted to their own product, the Hyperpublic staff has racked up some big karma. The members of the team have the following karma totals:
+
+Doug – 2349 points
+Jordan – 2102 points
+Eric – 2001 points
+Jonathan – 1747 points
+Amazingly, they've all accomplished these totals in the minimum number of tasks possible in order to reach each amount. For example, if their total was 6 points they would have accomplished this in just 2 tasks (2 "Add Thing" tasks), as opposed to accomplishing it by 3 "Add Place" tasks. Your job is to compute how many total tasks each user has completed. After you've done so, find the answer to Problem 2 using the following formula:
+
+Problem 2 Answer = Doug's total tasks * Jordan's total tasks * Eric's total tasks * Jonathan's total tasks
+*/
+    class User
+    {
+        string name;
+        int points;
+        int tasks;
+        public User(string n, int p)
+        {
+            name = n;
+            points = p;
+            tasks = calcTasks(points); // calcGreedyTasks(points);
+        }
+        private static int calcTask(int points, int value)
+        {
+            if (points < value) return 0;
+            else if (points > value) return points / value;
+            else return 1;
+        }
+
+        public int calcTasks(int points)
+        {
+            int max_f = calcTask(points, 2);
+            int lowest = max_f;
+            // I could assign weights and dynamic program but brute force seems faster
+            for (int f = 0; f <= 5; f++) 
+            {
+                for (int e = 0; e <= 5; e++)
+                {
+                    for (int d = 0; d <= 5; d++)
+                    {
+                        for (int c = 0; c <= 5; c++)
+                        {
+                            for (int b = 0; b <= 5; b++)
+                            {
+                                for (int a = 0; a <= calcTask(points, 98); a++)
+                                {
+                                    if (points - (f * 2) - (e * 3) - (d * 17) - (c * 23) - (b * 42) - (a * 98) == 0)
+                                    {
+                                        int total = a + b + c + d + e + f;
+                                        if (total < lowest) lowest = total;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+            return lowest;
+        }
+
+        // Originally wrote this beast, it didn't yield the results I was looking for
+        public int calcGreedyTasks(int points)
+        {
+            int facebook = calcTask(points, 98); // facebook share
+            int twitter = calcTask(points - (facebook * 98), 42);
+            int tag = calcTask(points - (facebook * 98) - (twitter * 42), 23);
+            int photo = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23), 17);
+            int thing = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17), 3);
+            int place = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17) - (thing * 3), 2);
+            int rem = points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17) - (thing * 3) - (place * 2);
+            if (rem != 0)
+            {
+                if (thing > 0)
+                {
+                    thing--;
+                    place = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17) - (thing * 3), 2);
+                }
+                else if (photo > 0)
+                {
+                    photo--;
+                    thing = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17), 3);
+                    place = calcTask(points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17) - (thing * 3), 2);
+                }
+                rem = points - (facebook * 98) - (twitter * 42) - (tag * 23) - (photo * 17) - (thing * 3) - (place * 2);
+            }
+
+            return facebook + twitter + tag + photo + thing + place;
+        }
+        public int getTasks()
+        {
+            return tasks;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            
+            users.Add(new User("Doug", 2349)); // 28
+            users.Add(new User("Jordan", 2102)); // 23 
+            users.Add(new User("Eric", 2001)); // 25
+            users.Add(new User("Johnathan", 1747)); // 22
+
+            int answer = 1;
+            foreach (User u in users)
+            {
+                Console.WriteLine(u.getTasks());
+                answer *= u.getTasks();
+            }
+            Console.WriteLine("Answer = " + answer); // 354200 = 28 * 23 * 25 * 22
+            Console.ReadKey();
+        }
+    }
+}
